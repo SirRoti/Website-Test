b0fa6dff62b278534ae47bf7e6c4de51baee99b4
sirroti <timgoellner6@gmail.com>
2022-07-31 12:22:45 +0200

    Changed behavior of main site buttons

--- functions.js
+++ functions.js
@@ -1,4 +1,10 @@
-window.onload = function () { discord_copy_text_out(); }
+window.onload = function () {
+    if (document.title.startsWith("Home")) {
+        main_middle_programming_out();
+        main_middle_minecraft_out();
+    }
+    else if (document.title.startsWith("Discord")) { discord_copy_text_out(); }
+}
 
 function discord_copy_profile() {
     navigator.clipboard.writeText('SirRoti#8706');
@@ -19,4 +25,21 @@ function discord_copy_text_out() {
         help_text.style.color = "#ffffff";
     } catch {}
     document.getElementById("copy_help_text").style.opacity = 0;
+}
+
+function main_middle_programming() {
+    document.getElementById("main-text-programing").style.opacity = 1;
+    document.getElementById("main-img-programing").style.color = "bisque";
+}
+function main_middle_programming_out() {
+    document.getElementById("main-text-programing").style.opacity = 0;
+    document.getElementById("main-img-programing").style.color = "white";
+}
+function main_middle_minecraft() {
+    document.getElementById("main-text-minecraft").style.opacity = 1;
+    document.getElementById("main-img-minecraft").style.color = "bisque";
+}
+function main_middle_minecraft_out() {
+    document.getElementById("main-text-minecraft").style.opacity = 0;
+    document.getElementById("main-img-minecraft").style.color = "white";
 }
